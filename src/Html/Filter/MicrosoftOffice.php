9d7b35bf40baaa6fb0959e3736e5a6b9b77a0eb9
Kieran Brahney <kieran@supportpal.com>
2023-09-07 16:29:44 +0100

    Fix conditional comments in Microsoft Office documents
    I think it's called downlevel-revealed conditional comments based on https://learn.microsoft.com/en-us/previous-versions/windows/internet-explorer/ie-developer/compatibility/ms537512(v=vs.85)?redirectedfrom=MSDN. The issue happens on libxml 2.9.14 as it reads it as an invalid <head> element, and moves it to <body>. It works OK in libxml 2.9.13

--- src/Html/Filter/MicrosoftOffice.php
+++ src/Html/Filter/MicrosoftOffice.php
@@ -5,6 +5,8 @@ namespace SupportPal\DomUtils\Html\Filter;
 use function str_replace;
 use function strpos;
 
+use const LIBXML_DOTTED_VERSION;
+
 class MicrosoftOffice extends Filter
 {
     public function preProcess(string $text): string
@@ -13,7 +15,9 @@ class MicrosoftOffice extends Filter
             return $text;
         }
 
-        return $this->removeNamespacedParagraphs($text);
+        $text = $this->removeNamespacedParagraphs($text);
+
+        return $this->fixConditionalComments($text);
     }
 
     private function isOfficeDocument(string $html): bool
@@ -26,5 +30,15 @@ class MicrosoftOffice extends Filter
     private function removeNamespacedParagraphs(string $html): string
     {
         return str_replace(['<o:p>', '</o:p>'], '', $html);
+    }
+
+    private function fixConditionalComments(string $html): string
+    {
+        if (LIBXML_DOTTED_VERSION === '2.9.14') {
+            $html = preg_replace('/<!(\[if\s*[^\[\]]+\])>/', '<!--$1-->', $html);
+            $html = preg_replace('/<!(\[endif\])>/', '<!--$1-->', $html);
+        }
+
+        return $html;
     }
 }
--- test/Html/Filter/MicrosoftOfficeTest.php
+++ test/Html/Filter/MicrosoftOfficeTest.php
@@ -45,5 +45,27 @@ class MicrosoftOfficeTest extends TestCase
 <body>Foo</body>
 </html>'
         ];
+
+        yield [
+            '<html xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:w="urn:schemas-microsoft-com:office:word" xmlns:m="http://schemas.microsoft.com/office/2004/12/omml" xmlns="http://www.w3.org/TR/REC-html40"><head>
+<![if !supportAnnotations]><style id="dynCom" type="text/css"><!-- --></style><![endif]>
+</head>
+</html>',
+            '<html xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:w="urn:schemas-microsoft-com:office:word" xmlns:m="http://schemas.microsoft.com/office/2004/12/omml" xmlns="http://www.w3.org/TR/REC-html40"><head>
+<!--[if !supportAnnotations]--><style id="dynCom" type="text/css"><!-- --></style><!--[endif]-->
+</head>
+</html>',
+        ];
+
+        yield [
+            '<html xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:w="urn:schemas-microsoft-com:office:word" xmlns:m="http://schemas.microsoft.com/office/2004/12/omml" xmlns="http://www.w3.org/TR/REC-html40"><head>
+<![if mso 9]><style id="dynCom" type="text/css"><!-- --></style><![endif]>
+</head>
+</html>',
+            '<html xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:w="urn:schemas-microsoft-com:office:word" xmlns:m="http://schemas.microsoft.com/office/2004/12/omml" xmlns="http://www.w3.org/TR/REC-html40"><head>
+<!--[if mso 9]--><style id="dynCom" type="text/css"><!-- --></style><!--[endif]-->
+</head>
+</html>',
+        ];
     }
 }
